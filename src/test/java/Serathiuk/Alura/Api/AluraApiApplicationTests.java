7ce04c4c7a71d7629305d6a485e704a617044782
kuihtares <dudu@serathiuk.com>
2023-04-01 10:20:00 -0300

    Alura Api

---
+++ src/test/java/Serathiuk/Alura/Api/AluraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Serathiuk.Alura.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
