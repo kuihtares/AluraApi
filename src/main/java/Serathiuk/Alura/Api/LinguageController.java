06808b95c2ce5fdee78dfdd0a806773cc83ba97a
Carlos Serathiuk <114114856+kuihtares@users.noreply.github.com>
2023-04-01 10:44:27 -0300

    Update LinguageController.java

--- src/main/java/Serathiuk/Alura/Api/LinguageController.java
+++ src/main/java/Serathiuk/Alura/Api/LinguageController.java
@@ -1,5 +1,13 @@
 package Serathiuk.Alura.Api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LinguageController {
 
+    @GetMapping(value="/preferred-language")
+    public String processLanguage () {
+        return "Oi,java!";
+    }
 }
